83606bbd9e0377243799b260298922d0f7c16702
hbauhvagner <hbauhvagner@gmail.com>
2023-09-07 23:10:58 -0300

    Exibindo erros no Material Ui

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,6 +7,7 @@ export default function FormularioCadastro({ aoEnviar }) {
     const [cpf, setCpf] = useState('');
     const [promocoes, setPromocoes] = useState(true);
     const [novidades, setNovidades] = useState(true);
+    const [erros, setErros] = useState({cpf: {valido: true, texto: ''}});
 
     return (
         <form onSubmit={event => {
@@ -40,6 +41,12 @@ export default function FormularioCadastro({ aoEnviar }) {
                 onChange={event => {
                     setCpf(event.target.value);
                 }}
+
+                onBlur={event => {
+                    setErros({cpf:{valido:false, texto:"CPF deve ter 11 digitos"}})
+                }}
+                error={!erros.cpf.valido}
+                helperText={erros.cpf.texto}
                 id="cpf" 
                 label="CPF" 
                 margin="normal" 
